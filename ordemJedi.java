e5850483365557fc4e33c523671de2ac7ccfdc33
PedroLunkes19 <robertoalvares717@gmail.com>
2025-07-02 10:08:45 -0300

    .

---
+++ ordemJedi.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+
+public class ordemJedi {
+    
+    final static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n;
+        int x;
+        int cont = 1;
+        // int qtdAceitos;
+        int[] vet;
+        
+        // while(sc.hasNext()){
+        //     n = sc.nextInt();
+        //     qtdAceitos = sc.nextInt();
+        //     vet = new int[n];
+
+        //     for (int i = 0; i < n; i++) {
+        //         vet[i] = sc.nextInt();
+        //     }
+
+        //     System.out.println(calcularForcaMin(vet, qtdAceitos));
+        // }
+
+        
+        while(true){
+            n = sc.nextInt();
+            x = sc.nextInt();
+            vet = new int[n];
+            if(x == 0 && n == 0){
+                break;
+            }
+
+            for (int i = 0; i < n; i++) {
+                vet[i] = sc.nextInt();
+            }
+
+            mergeSort(vet, 0, vet.length-1);
+
+            for (int i = 0; i < x; i++) {
+                System.out.println("CASE# " + cont + ":");
+                System.out.println(encontrarCristais(vet, x));
+            }
+            
+        }
+        
+
+    }
+
+
+    public static String encontrarCristais(int[] vet , int x){
+        for (int i = 0; i < vet.length; i++) {
+            if(vet[i] == x){
+                return (x + " found at " + (i+1));
+            }
+        }
+
+        return x + " not found";
+    }
+
+    public static int calcularForcaMin(int[] vet , int qtdAceitos){
+        mergeSort(vet, 0, vet.length);
+        int minForca = 0;
+
+        for (int i = 0; i < qtdAceitos; i++) {
+            minForca += vet[vet.length-1-i];
+        }
+
+        return minForca;
+    }
+
+    public static void mergeSort(int[] vet, int inicio, int fim) {
+        int meio = 0;
+
+        if (inicio < fim-1) {
+            meio = (inicio + fim) / 2;
+            mergeSort(vet, inicio, meio);
+            mergeSort(vet, meio + 1, fim);
+            intercalar(vet, inicio, meio, fim);
+        }
+
+    }
+
+    public static void intercalar(int vet[], int inicio, int meio, int fim) {
+        int i = inicio, j = meio, k = 0;
+        int[] vetAux = new int[fim - inicio];
+
+        while (i < meio && j < fim) {
+            if (vet[i] <= vet[j]) {
+                vetAux[k++] = vet[i++];
+            } else {
+                vetAux[k++] = vet[j++];
+            }
+
+        }
+
+        while (i < meio) {
+            vetAux[k++] = vet[i++];
+        }
+
+        while (j < fim) {
+            vetAux[k++] = vet[j++];
+        }
+
+        for (i = inicio; i < fim; i++) {
+            vet[i] = vetAux[i - inicio];
+           }
+    }
+
+}
+
